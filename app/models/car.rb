f1eb8d4b43904616f8bf82a6f49d94f8e6bc26b9
James Clement <jameseclement@gmail.com>
2019-01-15 13:42:16 -0500

    exit clear
    q
    
    a
    q
    
    exit
    end

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,28 @@
 class Car
 
-  attr_reader :make, :model
+  attr_reader :make, :model, :classification, :owner, :mechanic
+  @@all = []
+  @@classifications = []
+    def initialize(make, model, classification, owner, mechanic)
+      @make = make
+      @model = model
+      @classification = classification
+      @owner = owner
+      @mechanic = mechanic
+      @@classifications << classification
+      @@all << self
+    end
 
-  def initialize(make, model, classification)
-    @make = make
-    @model = model
-    @classification = classification
-  end
+    def self.all
+      @@all
+    end
 
+    def self.classifications
+      @@classifications
+    end
+
+    def classification_mechanics
+      Mechanic.all.select{|mechanic|
+        mechanic.specialty == self.classification}
+    end
 end
