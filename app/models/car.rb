2aa9a5483d503190a22e66f0c7bfcf17eb873229
Heloise Taillet <heloise96@hotmail.com>
2019-01-16 18:00:33 -0500

    passes all tests

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,28 @@
 class Car
 
-  attr_reader :make, :model
+  attr_accessor :classification
+  attr_reader :make, :model, :owner, :mechanic
+  @@all = []
 
-  def initialize(make, model, classification)
+  def initialize(make:, model:, classification:, owner:, mechanic:)
     @make = make
     @model = model
+    @owner = owner
+    @mechanic = mechanic
     @classification = classification
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.classifications
+    self.all.collect {|car| car.classification}.uniq
+  end
+
+  def mech_by_classification
+    Mechanic.all.select {|mechanic| mechanic.specialty == self.classification}
   end
 
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,26 @@
 class Mechanic
 
   attr_reader :name, :specialty
+  @@all = []
+  def initialize(name:, specialty:)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  def self.all
+    @@all
+  end
 
+  def mechanics_cars
+    Car.all.select {|car| car.mechanic == self}
+  end
 
+  def mechanics_owners
+    self.mechanics_cars.collect {|car| car.owner}
+  end
+
+  def mechanics_ownernames
+    self.mechanics_cars.collect {|car| car.owner.name}
+  end
 end
