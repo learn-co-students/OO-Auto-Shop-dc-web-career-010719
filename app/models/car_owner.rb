6b273beeef9b5a69eb26c3871c0b8d836b2ba46f
shannon-nabors <shannon.nabors@gmail.com>
2019-01-17 11:50:25 -0500

    Done

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,38 @@
 class CarOwner
 
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def buy_car(make:, model:, classification:)
+    Car.new(make:make, model:model, classification:classification, owner:self)
+  end
+
+  def select_mechanic(car_make:, car_model:, mechanic_name:)
+    car = self.cars.find { |car| car.make == car_make && car.model == car_model }
+    mechanic = Mechanic.all.find { |mech| mech.name == mechanic_name }
+    car.mechanic = mechanic
+  end
+
+  def cars
+    Car.all.select { |c| c.owner == self }
+  end
+
+  def mechanics
+    self.cars.map { |c| c.mechanic }
+  end
+
+  def self.avg_car_amount
+    car_nums = self.all.map { |o| o.cars.size }
+    (car_nums.inject(:+).to_f/(car_nums.size)).round(2)
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,22 @@ def reload
   load 'config/environment.rb'
 end
 
+shannon = CarOwner.new("Shannon")
+matt = CarOwner.new("Matt")
+ryan = CarOwner.new("Ryan")
+kyle = Mechanic.new(name:"Kyle", specialty:"electric sports car")
+chris = Mechanic.new(specialty:"sedan", name:"Chris")
+phil = Mechanic.new(specialty:"sedan", name:"Phil")
+
+accord = shannon.buy_car(make:"Honda", model:"Accord", classification:"sedan")
+outback = shannon.buy_car(make:"Subaru", model:"Outback", classification:"hatchback")
+ryan.buy_car(make:"BMW", model:"I8", classification:"electric sports car")
+matt.buy_car(make:"Tesla", model:"S", classification:"electric sports car")
+shannon.select_mechanic(car_make:"Honda", car_model:"Accord", mechanic_name:"Chris")
+shannon.select_mechanic(car_make:"Subaru", car_model:"Outback", mechanic_name:"Chris")
+ryan.select_mechanic(car_make:"BMW", car_model:"I8", mechanic_name:"Kyle")
+matt.select_mechanic(car_make:"Tesla", car_model:"S", mechanic_name:"Kyle")
+
 binding.pry
+
+puts "Table 6 rules"
